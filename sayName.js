b2df65d5e808e1758a76a9bf98493802fe4aa1bb
scoobycoder <scoobycoder@gmail.com>
2016-07-23 15:54:25 -0400

    extract luke dialog

--- sayName.js
+++ sayName.js
@@ -144,12 +144,9 @@ function setFirstName(intent, session, callback) {
         sessionAttributes = createFirstNameAttributes(myFirstName);
         speechOutput = "I now know your name is " + myFirstName;
 
-        if (firstName == "Luke") {
-            speechOutput += ". " +
-            "It is a period of civil war. Rebel spaceships, striking from a hidden base, have won their first victory against the evil Galactic Empire."
- + " During the battle, Rebel spies managed to steal secret plans to the Empire's ultimate weapon, the DEATH STAR, an armored space station with enough power to destroy an entire planet."
-+ " Pursued by the Empire's sinister agents, Princess Leia races home aboard her starship, custodian of the stolen plans that can save her people and restore freedom to the galaxy.";
-
+        if (myFirstName == "Luke") {
+            speechOutput += ". " + luke();
+            
         }
 
 
@@ -163,6 +160,13 @@ function setFirstName(intent, session, callback) {
 
     callback(sessionAttributes,
          buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession));
+}
+
+function luke(){
+    return "It is a period of civil war. Rebel spaceships, striking from a hidden base, have won their first victory against the evil Galactic Empire."
+ + " During the battle, Rebel spies managed to steal secret plans to the Empire's ultimate weapon, the DEATH STAR, an armored space station with enough power to destroy an entire planet."
++ " Pursued by the Empire's sinister agents, Princess Leia races home aboard her starship, custodian of the stolen plans that can save her people and restore freedom to the galaxy.";
+
 }
 
 function createFirstNameAttributes(myFirstName) {
